5e5dd78b9b6e8a4cfbdb0b4ba7938b66a9799311
Kashyap Kannan <kashkann@amazon.com>
2025-01-03 23:46:11 +0530

    Generate, cache and pass valid fernet keys for local deployment (#196)

--- quality-checks/lint_bash.sh
+++ quality-checks/lint_bash.sh
@@ -8,10 +8,9 @@ if [[ "$PWD" != "$REPO_ROOT" ]]; then
     echo "The script must be run from the repo root. Please cd into the repo root directory and type: ./quality-checks/${SCRIPT_NAME}"
     exit 1
 fi
-
-# Lint all Bash files
+# Lint all Bash files, excluding .venv directory
 echo "Running ShellCheck on Bash scripts..."
-if ! find . -type f -name "*.sh" -exec shellcheck {} +; then
+if ! find . -type f -name "*.sh" -not -path "./.venv/*" -exec shellcheck {} +; then
     echo "ShellCheck linting failed."
     exit 1
 else
